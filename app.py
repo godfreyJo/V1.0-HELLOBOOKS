0bc52255dbb868fcbca952a473cbcf3d48046811
godfreyJo <godfreyoyugi@gmail.com>
2020-03-29 20:47:00 +0300

    review 2

--- app.py
+++ app.py
@@ -1,60 +1,67 @@
-from flask import Flask, jsonify, request, url_for, render_template
+from flask import Flask, jsonify, request, url_for, render_template, redirect
+from forms import RegistrationForm, LoginForm
 from flask_classy import FlaskView, route
 
 
 app = Flask(__name__)
 
 books = [{'Title': 'Microeconomics'}, {'Title':'Primary English'}, {'Title':'Primary Maths'}]
-users = [{'Username': 'John','password':'password'},
-        {'Username': 'Joel','password':'password'},
-        {'Username': 'Joan','password':'password'},
-        {'Username': 'Job','password':'password'}
-        ]
+# users = [{'Username': 'John','password':'password'},
+#         {'Username': 'Joel','password':'password'},
+#         {'Username': 'Joan','password':'password'},
+#         {'Username': 'Job','password':'password'}
+#         ]
 
 
-#Class view for all the User funvtionalities
+# #Class view for all the User funvtionalities
 
-class UserView(FlaskView):
+# class UserView(FlaskView):
     
 #Function for viewing  all the users
-    @app.route('/users', methods=['GET'])
-    def allusers():
-        return render_template("user.html");
+@app.route('/')
+@app.route("/home")
+def home():
+    return render_template('home.html', books = books)
+
+
+# @app.route('/users', methods=['GET'])
+# def allusers():
+#     return render_template("user.html");
         # return jsonify({'users': users})
     
-#Function for username in the list of all usernames   
-    @app.route('/users/<string:name>', methods=['GET'])
-    def Usersearch(self, name):
-        Usearch = [user for user in users if user['Username']== name]
-        return jsonify({'user': Usearch[0]})
 
- #This function below adds a new user inform of dictionary into the existing dictionary   
-    @app.route('/users', methods=['POST'])
-    def adduser(self, users):
-        user = {'Username': request.json['Username'],
-           'password': request.json['password']}
-        users.append(user)
-        return jsonify({'users': users})
+#     @app.route('/users/<string:name>', methods=['GET'])
+#     def Usersearch(self, name):
+#         Usearch = [user for user in users if user['Username']== name]
+#         return jsonify({'user': Usearch[0]})
+
+#  #This function below adds a new user inform of dictionary into the existing dictionary   
+#     @app.route('/users', methods=['POST'])
+#     def adduser(self, users):
+#         user = {'Username': request.json['Username'],
+#            'password': request.json['password']}
+#         users.append(user)
+#         return jsonify({'users': users})
  
- #this function updates the existing username dictionary 
-    @app.route('/users/<string:name>', methods=['PUT'])
-    def UserEdit(self, username):
-        Usearch = [user for user in users if user['Username']== name]
-        Usearch [0]['name'] = request.json['Username']
-        return jsonify({'user': Usearch[0]})
+#  #this function updates the existing username dictionary 
+#     @app.route('/users/<string:name>', methods=['PUT'])
+#     def UserEdit(self, username):
+#         Usearch = [user for user in users if user['Username']== name]
+#         Usearch [0]['name'] = request.json['Username']
+#         return jsonify({'user': Usearch[0]})
     
     
-#Create a class for viewing various books functionalities
-class BooksView(FlaskView):
+# #Create a class for viewing various books functionalities
+# class BooksView(FlaskView):
     
-    @app.route('/', methods=['GET'])
-    def test():
-        return jsonify({'message':'trial!'})
+#     @app.route('/', methods=['GET'])
+#     def test():
+#         return jsonify({'message':'trial!'})
 
 
-    @app.route('/books', methods=['GET'])
-    def allbooks(self):
-        return jsonify({'books': books})
+#     @app.route('/books', methods=['GET'])
+#     def allbooks(self):
+#         return jsonify({'books': books})
 
     
 
