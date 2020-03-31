5a8fd15a9ea469bd7e527b2f391f1070d3119725
godfreyJo <godfreyoyugi@gmail.com>
2020-03-31 23:13:06 +0300

    adding the forms

--- forms.py
+++ forms.py
@@ -11,6 +11,16 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password',validators = [DataRequired()])
 
     confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField(
+        'Email'=[DataRequired(),Email()])
+    password = PasswordField(
+        'Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit  = SubmitField('Login')
          
     
 
