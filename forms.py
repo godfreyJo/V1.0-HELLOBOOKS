1861e9d1db33430c0898588144bc4ef97eed2e24
godfreyJo <godfreyoyugi@gmail.com>
2020-04-01 23:24:40 +0300

    furnish1april1

--- forms.py
+++ forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_wtf.file import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+
 
 
 class RegistrationForm(FlaskForm):
@@ -14,13 +17,26 @@ class RegistrationForm(FlaskForm):
 
     submit = SubmitField('Sign Up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken.Please choose a different one.')
+
+
 class LoginForm(FlaskForm):
     email = StringField(
-        'Email'=[DataRequired(),Email()])
+                        'Email'=[DataRequired(),Email()])
     password = PasswordField(
-        'Password', validators=[DataRequired()])
+                        'Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit  = SubmitField('Login')
+
+
          
     
 
