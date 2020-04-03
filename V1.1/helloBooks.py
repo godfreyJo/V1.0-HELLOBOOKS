f933960e70799417b7279562894ab94d27beee49
godfreyJo <godfreyoyugi@gmail.com>
2020-04-03 19:37:09 +0300

    database

--- V1.1/helloBooks.py
+++ V1.1/helloBooks.py
@@ -1,17 +1,35 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
+
+app = Flask(__name__)
 app.config['SECRET_KEY'] = '67397d27f41e16d65a3d00e10112b4e0'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///HelloBooksWebsite.db'
+
 db = SQLAlchemy(app)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False. default='default.jpg')
+    password = db.Column(db.String(60),  nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    author = db.Column(db.String(20), unique=True, nullable=False)
+    date_posted = db.Column(db.DateTime, unique=True, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.title}', '{self.author}')"
 
 books = [
     {
