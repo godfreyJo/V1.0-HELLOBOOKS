bd812aa5d8bf944118ca932f3db0e4f2aeb1ef22
godfreyJo <godfreyoyugi@gmail.com>
2020-04-10 06:39:52 +0300

    sending an email

--- V1.1/helloBooks/forms.py
+++ V1.1/helloBooks/forms.py
@@ -62,3 +62,21 @@ class BookForm(FlaskForm):
     submit = SubmitField('Post')
 
 
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You will have to register first')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
+
+
+
+
+
--- V1.1/helloBooks/routes.py
+++ V1.1/helloBooks/routes.py
@@ -2,10 +2,11 @@ import os
 import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
-from helloBooks import app, db, bcrypt
-from helloBooks.forms import RegistrationForm, LoginForm, UpdateProfileForm, BookForm
+from helloBooks import app, db, bcrypt, mail
+from helloBooks.forms import RegistrationForm, LoginForm, UpdateProfileForm, BookForm, RequestResetForm, ResetPasswordForm
 from helloBooks.models import User, Book
 from flask_login import login_user, current_user, logout_user, login_required
+from flask_mail import Message
 
 
 
@@ -152,8 +153,47 @@ def user_books(username):
     books = Book.query.filter_by(author=user).order_by(Book.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('user_books.html', books=books, user=user)
 
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request', sender='godfreyoyugi@gmail.com.com', recipients=[user.email])
+    msg.body = f''' To reset your password, visit the following link
+{url_for('reset_token', token=token, _external=True)}
+    If you never made this request then ignore this email
+     '''
+    mail.send(msg)
 
 
+    
+
+@app.route("/reset_password/", methods=['GET', 'POST'])
+def reset_password(): 
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()  
+        send_reset_email(user)  
+        flash('An email has been sent with instructions', 'info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', title='Reset Password', form=form)
+
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token): 
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token', 'warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')        
+        user.password = hashed_password
+        db.session.commit()
+        flash(f'Your password has been updated: You are now able to log in!', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', title='Reset Password', form=form)
 
 
 
